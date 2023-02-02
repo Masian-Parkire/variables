5322743bc60bb70a76e4dfc92f14a32ac14af3ca
Masian Parkire <parkiremasiam@gmail.com>
2023-02-02 10:02:04 +0300

    added variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var  fullname = "masian"
+    var age = 22
+    var  phonenumber = "0791642728"
+    var weightinKg = 56
+    var isKenyan: Boolean = true
+    var notKenyan: Boolean = false
+
+    println(isKenyan)
+
+
+}
+
